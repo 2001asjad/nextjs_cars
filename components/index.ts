b171243d40013517d69b94f9e3ae0399f038095c
Asjad Ahmed Khan <asjadahmedkhan@Asjads-MacBook-Air.local>
2023-07-03 17:12:58 +0530

    first commit

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    CustomButton,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore
+}
